10a4c08769e4315d2eb714b97e08f089bb6ea606
380450382@qq.com <380450382@qq.com>
2020-02-21 17:01:47 +0800

    start no sub info

--- src/main/java/com/book/command/execute/RealTimelOffAllExecute.java
+++ src/main/java/com/book/command/execute/RealTimelOffAllExecute.java
@@ -7,7 +7,12 @@ public final class RealTimelOffAllExecute<R> implements Function<String,R> {
     @Override
     public R apply(String arg) {
         Map<String, RealTimelOnExecute.SubState> stopMap = RealTimelOnExecute.getInstance().getStopMap();
-        stopMap.keySet().forEach(key -> stopMap.get(key).current.interrupt());
+        stopMap.keySet().forEach(key -> {
+            Thread current = stopMap.get(key).current;
+            if(current != null){
+                current.interrupt();
+            }
+        });
         return null;
     }
 }
